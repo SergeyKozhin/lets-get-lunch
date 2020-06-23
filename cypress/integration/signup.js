529ab11166a5ee045cd8613b4e98e9eec9ca4d8a
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-06-23 20:42:04 +0300

    Added e2e tests for navbar

--- cypress/integration/signup.js
+++ cypress/integration/signup.js
@@ -39,4 +39,27 @@ describe('Signup', () => {
       .should('be.visible')
       .should('have.text', 'Your password must be at least 5 characters long.')
   });
+
+  it('should display an error for a username that already exists', () => {
+    cy
+      .visit('/signup')
+      .url().should('include', '/signup')
+      .get('#username').type('user')
+      .get('#password').type('password')
+      .get('#BBQ').click()
+      .get('form').submit()
+      .url().should('include', '/dashboard');
+
+    cy.get('[data-test=logout]').click();
+
+    cy
+      .visit('/signup')
+      .url().should('include', '/signup')
+      .get('#username').type('user')
+      .get('#password').type('pswd')
+      .get('form').submit()
+      .get('.alert')
+      .should('be.visible')
+      .should('have.text', 'This user already exists.')
+  });
 })
